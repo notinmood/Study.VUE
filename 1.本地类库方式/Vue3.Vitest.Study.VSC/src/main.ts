f1128d8430a50d4a975fc6808a62f456a2fd7af7
xiedali <9727005@qq.com>
2025-01-10 19:49:23 +0800

    格式化

--- 1.本地类库方式/Vue3.Vitest.Study.VSC/src/main.ts
+++ 1.本地类库方式/Vue3.Vitest.Study.VSC/src/main.ts
@@ -1,3 +1,13 @@
+/*
+ * @Author       : Shandong Xiedali
+ * @Mail         : 9727005@qq.com
+ * @Date         : 2024-12-19 09:00:29
+ * @LastEditors  : Shandong Xiedali
+ * @LastEditTime : 2025-01-10 19:48:16
+ * @FilePath     : main.ts
+ * @Description  :
+ * Copyright (c) 2025 by Hiland & RainyTop, All Rights Reserved.
+ */
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -6,6 +16,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//创建vue应用
 const app = createApp(App)
 
 app.use(createPinia())
